7f431c7ddf7ee380e98b406fcf3cf3b8f98ab528
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2023-05-20 00:10:55 +0200

    chore: update nx & angular to v16

---
+++ apps/demo/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig } from '@angular/core';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+import { appRoutes } from './app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
+};
--- apps/demo/src/main.ts
+++ apps/demo/src/main.ts
@@ -1,13 +1,8 @@
+import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), importProvidersFrom(BrowserAnimationsModule)],
-}).catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
