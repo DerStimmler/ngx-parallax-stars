21df168bf46f19ef68e5df4500245209728832a5
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2025-01-06 21:13:14 +0100

    refactor: apply route lazy loading migrations

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Route } from '@angular/router';
-import { MainComponent } from './main/main.component';
+
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    component: MainComponent,
+    loadComponent: () => import('./main/main.component').then(m => m.MainComponent),
   },
 ];
