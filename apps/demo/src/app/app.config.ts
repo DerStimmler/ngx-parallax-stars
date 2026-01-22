18c7c6f66b75050454765ae333035d516032dac0
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2026-01-22 21:37:37 +0100

    fix: bugs after nx update

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,12 +1,7 @@
-import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    importProvidersFrom(BrowserAnimationsModule),
-    provideExperimentalZonelessChangeDetection(),
-  ],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideZonelessChangeDetection()],
 };
